b0f0020a01b3a2b3181b218e2c33c599f4f21fdb
ZabihullahRPL11 <zaachaf@gmail.com>
2023-02-17 15:09:00 +0700

    first commit

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,21 @@
+<?php 
+    $kata1 = "Topi";
+    $kata2 = "Bundar";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>a</title>
+</head>
+<body>
+    <h2>
+        <i>
+            <?php echo "\"$kata1 saya $kata2," ?>
+            <?php echo "$kata2 $kata1 saya.\"" ?>  
+        </i>
+    </h2>
+</body>
+</html>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,23 @@
+<?php 
+    $npm = 19;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>b</title>
+</head>
+<body>
+    <p>
+        Aku adalah angka <b><?php echo $npm ?></b><br>
+        Jika aku di kali 5, maka aku sekarang menjadi <b><?php echo $npm * 5 ?></b><br>
+        Jika aku di bagi 2, maka aku sekarang menjadi <b><?php echo 95 / 2 ?></b><br>
+        Jika aku di tambah 75, maka aku sekarang menjadi <b><?php echo 47.5 + 75 ?></b><br>
+        Jika aku di dikurang 20, maka aku sekarang menjadi <b><?php echo 122.5 - 20 ?></b><br>
+    </p>
+   
+
+</body>
+</html>
