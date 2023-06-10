87f86fb77466582729250b26a023b5c407f02256
ZabihullahRPL11 <zaachaf@gmail.com>
2023-06-10 19:28:58 +0700

    update live search

--- tubes/search.php
+++ tubes/search.php
@@ -39,8 +39,8 @@ include 'components/add_cart.php';
 
 <section class="search-form">
    <form method="post" action="">
-      <input type="search" name="search_box" placeholder="search here..." class="box">
-      <button type="submit" name="search_btn" class="fas fa-search"></button>
+      <input type="text" id="keyword" name="keyword" placeholder="search here..." class="box">
+      <button type="button" name="search_btn" class="fas fa-search"></button>
    </form>
 </section>
 
@@ -48,66 +48,24 @@ include 'components/add_cart.php';
 
 
 <section class="products" style="min-height: 100vh; padding-top:0;">
-
-<div class="box-container">
-
-      <?php
-         if(isset($_POST['search_box']) OR isset($_POST['search_btn'])){
-         $search_box = $_POST['search_box'];
-         $select_products = $conn->prepare("SELECT * FROM `products` WHERE name LIKE '%{$search_box}%'");
-         $select_products->execute();
-         if($select_products->rowCount() > 0){
-            while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
-      ?>
-      <form action="" method="post" class="box">
-         <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
-         <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
-         <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
-         <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
-         <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
-         <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
-         <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
-         <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
-         <div class="name"><?= $fetch_products['name']; ?></div>
-         <div class="flex">
-            <div class="price"><span>Rp</span><?= $fetch_products['price']; ?></div>
-            <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
-         </div>
-      </form>
-      <?php
-            }
-         }else{
-            echo '<p class="empty">tidak ada produk!</p>';
-         }
-      }
-      ?>
-
-   </div>
-
+<div class="box-container" id="searchContainer">
+</div>
 </section>
-
-
-
-
-
-
-
-
-
-
-
 <!-- footer section starts  -->
 <?php include 'components/footer.php'; ?>
 <!-- footer section ends -->
-
-
-
-
-
-
-
 <!-- custom js file link  -->
 <script src="js/script.js"></script>
+<script>
+   const keyword = document.getElementById('keyword');
+   const searchContainer = document.getElementById('searchContainer');
+   keyword.onkeyup = function() {
+      // console.log(keyword.value);
+      fetch("ajax_search.php?keyword="+keyword.value)
+      .then((response)=>response.text())
+      .then((text)=>(searchContainer.innerHTML=text));
+   }
+</script>
 
 </body>
 </html>
